2d8bcac2692edc98af86432ba29122688f84c9bb
Jommel Toledo <111914360+Hearts017@users.noreply.github.com>
2023-02-24 12:09:48 +0800

    add something

--- src/App.tsx
+++ src/App.tsx
@@ -6,27 +6,31 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="./vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="card no-hover mb-2">
+      <ul className="list-group list-group-flush">
+        <li className="list-group-item">
+          <div className="row">
+            <div className="col-lg-3">
+              <div className="profile">
+             
+               
+              </div>
+            </div>
+            <div className="col-lg-6">
+              <div className="topic">
+                
+                
+              </div>
+            </div>
+            <div className="col-lg-3">
+              <div className="more-details">
+                <a href="" className='view-details'>View Details</a>
+                <a href="" className='social'><i className="bi bi-twitter"></i></a>
+              </div>
+            </div>
+          </div>
+        </li>
+      </ul>
     </div>
   )
 }
